37fdb091f614a288d911ac54356b5805c0472bd5
MartinKamenov <martinkamenov10@gmail.com>
2018-12-27 23:44:07 +0200

    Added engine object contracts

---
+++ src/app/src/main/java/com/games/mkvs/interstellarmanager/engine/models/game_objects/contracts/GameObject.java
@@ -0,0 +1,9 @@
+package com.games.mkvs.interstellarmanager.engine.models.game_objects.contracts;
+
+/**
+ * Created by Martin on 27.12.2018 г..
+ */
+
+public interface GameObject {
+    void update();
+}
---
+++ src/app/src/main/java/com/games/mkvs/interstellarmanager/engine/models/game_objects/contracts/MovingDirection.java
@@ -0,0 +1,14 @@
+package com.games.mkvs.interstellarmanager.engine.models.game_objects.contracts;
+
+/**
+ * Created by Martin on 27.12.2018 г..
+ */
+
+public enum MovingDirection {
+    Left,
+    Up,
+    Right,
+    Down,
+    Further,
+    Closer
+}
