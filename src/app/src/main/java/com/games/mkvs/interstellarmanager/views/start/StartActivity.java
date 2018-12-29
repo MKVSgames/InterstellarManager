5f3bee22a64fd3ae8bfa56d175e8c09239eac88f
MartinKamenov <martinkamenov10@gmail.com>
2018-12-29 19:25:30 +0200

    Added constants and put game to whole screen

--- src/app/src/main/java/com/games/mkvs/interstellarmanager/views/start/StartActivity.java
+++ src/app/src/main/java/com/games/mkvs/interstellarmanager/views/start/StartActivity.java
@@ -1,15 +1,25 @@
 package com.games.mkvs.interstellarmanager.views.start;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.games.mkvs.interstellarmanager.R;
+import com.games.mkvs.interstellarmanager.constants.Constants;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Constants.SCREEN_WIDTH = dm.widthPixels;
+        Constants.SCREEN_HEIGHT = dm.heightPixels;
         setContentView(R.layout.activity_start);
     }
 }
