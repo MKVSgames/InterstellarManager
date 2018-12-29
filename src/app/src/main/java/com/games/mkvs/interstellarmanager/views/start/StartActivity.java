aae3f4bd0e9c839772809eb984ce864484f5b07d
Vinchenzo <vinchenco10@abv.bg>
2018-12-29 18:34:51 +0200

    Setup project

--- src/app/src/main/java/com/games/mkvs/interstellarmanager/views/start/StartActivity.java
+++ src/app/src/main/java/com/games/mkvs/interstellarmanager/views/start/StartActivity.java
@@ -1,4 +1,4 @@
-package com.games.mkvs.interstellarmanager.views;
+package com.games.mkvs.interstellarmanager.views.start;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ src/app/src/main/java/com/games/mkvs/interstellarmanager/views/start/StartPresenter.java
@@ -0,0 +1,4 @@
+package com.games.mkvs.interstellarmanager.views.start;
+
+public class StartPresenter {
+}
