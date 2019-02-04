5310e80488b4f7fd48cb633824b503cc5c7cebfb
MartinKamenov <martinkamenov10@gmail.com>
2019-02-04 19:53:21 +0200

    Added company repository as dependany for Overview presenter

--- src/app/src/main/java/com/games/mkvs/interstellarmanager/views/game_menu/overview/OverviewPresenter.java
+++ src/app/src/main/java/com/games/mkvs/interstellarmanager/views/game_menu/overview/OverviewPresenter.java
@@ -1,15 +1,17 @@
 package com.games.mkvs.interstellarmanager.views.game_menu.overview;
 
 import com.games.mkvs.interstellarmanager.base.contracts.BaseContracts;
+import com.games.mkvs.interstellarmanager.web.repositories.base.CompanyRepository;
 
 /**
  * Created by Martin on 3.1.2019 г..
  */
 
 public class OverviewPresenter implements OverviewContracts.IOverviewPresenter {
+    private final CompanyRepository companyRepository;
     private OverviewContracts.IOverviewView mView;
-    public OverviewPresenter() {
-
+    public OverviewPresenter(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
     }
     @Override
     public void subscribe(BaseContracts.View view) {
